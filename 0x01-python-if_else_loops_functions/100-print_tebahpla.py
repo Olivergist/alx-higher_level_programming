dc4e992d112f6c72609b8007812e1960df1c6eb6
Olivergist <victorolaonifogo@gmail.com>
2023-10-04 18:30:15 -0700

    Update

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# Initialize a variable 'i' to control the case change
+i = 0
+
+# Loop through the ASCII values of lowercase letters from 'z' to 'a'
+for c in range(ord('z'), ord('a') - 1, -1):
+    # Print the character converted to uppercase or lowercase based on 'i'
+    print("{}".format(chr(c - i)), end="")
+    
+    # Toggle 'i' between 32
+    i = 32 if i == 0 else 0
