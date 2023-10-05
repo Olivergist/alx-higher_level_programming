baa6b137000d1020ef4a27175c1e3dd0a7c6a2a0
Olivergist <victorolaonifogo@gmail.com>
2023-10-04 18:31:13 -0700

    Update

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -7,6 +7,5 @@ i = 0
 for c in range(ord('z'), ord('a') - 1, -1):
     # Print the character converted to uppercase or lowercase based on 'i'
     print("{}".format(chr(c - i)), end="")
-    
     # Toggle 'i' between 32
     i = 32 if i == 0 else 0
