21e3fa011df2f7cbb9becf622c156a7faa8b383a
Olivergist <victorolaonifogo@gmail.com>
2023-10-08 01:16:18 -0700

    Update

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 
-#We import the function
-import add_0
+# we import the module
+from add_0 import add
 
-#Assign the value
-a = 1
-b = 2
+def main():
+    # Assign thr values to store in a and b
+    a = 1
+    b = 2
 
-#print the result
-print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+    # We calculate the result using the imported function
+    result = add(a, b)
+
+    # Display the result
+    print("{} + {} = {}".format(a, b, result))
+
+    if __name__ == "__main__":
+        # Call the main function only when the script is run directly
+        main()
