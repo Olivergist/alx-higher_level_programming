8802fcc611081dce1fc62a30479ea851a2178098
Olivergist <victorolaonifogo@gmail.com>
2023-10-08 20:01:26 -0700

    Update

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,6 @@
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list  # Return the original list if idx is out of range or negative
+    else:
+        my_list[idx] = element
+        return my_list
