d82961a7ad00fc7a1424d243e508d87f6dee89d1
Olivergist <victorolaonifogo@gmail.com>
2023-10-11 11:19:26 -0700

    Update

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,4 +3,4 @@ def multiple_returns(sentence):
     if sentence == "":
         return (0, None) 
     # Return (0, None) if the sentence is empty
-    return (len(sentence), sentence[0])  
+    return (len(sentence), sentence[0])
