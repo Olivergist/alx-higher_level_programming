3247708df7a3987394e9ccec9f25c939e9af20bf
Olivergist <victorolaonifogo@gmail.com>
2023-10-11 15:27:11 -0700

    Update

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
-    """Check if elements in the list are divisible by 2."""
     multiples = []
 
-    # Iterate through the list and check if each element is by 2
+    # Iterate through the list
     for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
             multiples.append(True) 
