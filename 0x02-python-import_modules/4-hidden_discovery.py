24162aadf05bef7050ef2473f32517e4986dddbc
Olivergist <victorolaonifogo@gmail.com>
2023-10-11 18:51:42 -0700

    Update

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 
-# Check if this script is run as the main program
+# Check if this script is run
 if __name__ == "__main__":
     # Import the hidden_4 module
     import hidden_4
-    
-    # Get a list of all names defined in the hidden_4 module
+
+    # Get a list of all names defined
     names = dir(hidden_4)
 
 # Iterate through each name in the list
 for name in names:
-# Check if the name does not start with double underscores
+# Check if the name does not start
     if name[:2] != "__":
     # Print the public names
 print(name)
