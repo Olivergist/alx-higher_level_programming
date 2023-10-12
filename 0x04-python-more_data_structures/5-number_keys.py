4bf4d0f60d66c938ae4f13339ff0e612d8a4d595
Olivergist <victorolaonifogo@gmail.com>
2023-10-11 18:17:21 -0700

    Update

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
     star = 0
-    list_keys =list(a_dictionary.keys())
+    list_keys = list(a_dictionary.keys())
 
     for i in list_keys:
         star += 1
