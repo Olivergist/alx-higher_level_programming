cf5b3c1f752d5c506a8129238526f11096036ebd
Olivergist <victorolaonifogo@gmail.com>
2023-10-11 18:11:52 -0700

    Update

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    # Use the len() function to get the number
     star = 0
     list_keys =list(a_dictionary.keys())
 
     for i in list_keys:
         star += 1
 
-    return(star)
+    return (star)
