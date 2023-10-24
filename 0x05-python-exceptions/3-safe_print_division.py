2ba8c441cdab604965065f9e4be98b563641bda2
Olivergist <victorolaonifogo@gmail.com>
2023-10-24 02:13:01 +0100

    Update

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
 # functions that divides 2 int
+
 def safe_print_division(a, b):
 	try:
 		result = a / b
---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+#!/urs/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+	result = [0] * list_length
+	for i in range(list_length):
+		try:
+			if i < len(my_list_1) and i < len(my_list_2):
+				result[i] = my_list_1[i] / my_list_2[i]
+			else:
+				raise IndexError("out of range")
+		except (ZeroDivisionError, TypeError):
+			if isinstance(my_list_1[i], (int, float)) and isinstance(my_list_2[i], (int, float)):
+				print("division by 0")
+			else:
+				print("wrong type")
+		except IndexError:
+			print("out of range")
+		finally:
+			pass
+	return result
