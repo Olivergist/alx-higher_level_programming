9c6658291ff116c28bae56d30c9047d698369eb0
Olivergist <victorolaonifogo@gmail.com>
2023-10-29 13:06:37 +0000

    1-square Added

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Square with a private attribute"""
+
+
+class Square:
+    """
+    Initializes a new Square
+
+    Paremeters:
+    Size (int): The size of the square.
+
+    """
+
+
+def __init__(self, size):
+
+    self. __size = size
