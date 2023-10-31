599ad82fda7b0f18e6bf823adca9148f30834bb5
Olivergist <victorolaonifogo@gmail.com>
2023-10-31 17:08:16 +0000

    Corrected error

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """The LockedClass is a class that restricts the creation of new attributes."""
 
+
 class LockedClass:
-    """`__slots__ = ['first_name']` is a special attribute in Python that restricts the creation of new
+    """`__slots__ = ['first_name']` is a special attribute in
+    Python that restricts the creation of new
     attributes in instances of the class.
     """
     __slots__ = ['first_name']
